9664a98ede8ea101dea713d4c1a0dd1732051f73
AryanYadagiri <aryanyadagiri15@gmail.com>
2025-01-09 11:14:36 +0530

    pagination improved

--- app/api/services/route.js
+++ app/api/services/route.js
@@ -5,51 +5,31 @@ export async function GET(request) {
   try {
     const { searchParams } = new URL(request.url);
     const category = searchParams.get("category");
+    const whereClause = category === "all" ? {} : { categories: category };
     const cursor = searchParams.get("cursor");
-    let lastServiceInResults;
-    let myCursor;
-    if (!cursor) {
-      const firstQueryResults = await prisma.service.findMany({
-        take: 8,
-        where: {
-          categories: category,
-        },
-        orderBy: {
-          id: "asc",
-        },
-      });
-      if (firstQueryResults.length > 0) {
-        lastServiceInResults = firstQueryResults[firstQueryResults.length - 1];
-        myCursor = lastServiceInResults.id;
-      }
-      return NextResponse.json(
-        { items: firstQueryResults, cursor: myCursor },
-        { status: 200 }
-      );
-    }
 
-    const secondQueryResults = await prisma.service.findMany({
+    const queryResults = await prisma.service.findMany({
       take: 8,
-      skip: 1,
-      cursor: {
-        id: cursor,
-      },
-      where: {
-        categories: category,
-      },
+      where: whereClause,
       orderBy: {
         id: "asc",
       },
+      ...(cursor ? { cursor: { id: cursor }, skip: 1 } : {}),
     });
-
-    if (secondQueryResults.length > 0) {
-      lastServiceInResults = secondQueryResults[secondQueryResults.length - 1];
-      myCursor = lastServiceInResults.id;
+    if (queryResults.length === 8) {
+      const myCursor = queryResults[7].id;
+      return NextResponse.json(
+        { items: queryResults, cursor: myCursor },
+        { status: 200 }
+      );
+    } else if (queryResults.length < 8 && queryResults.length > 0) {
+      return NextResponse.json(
+        { items: queryResults, cursor: cursor },
+        { status: 200 }
+      );
+    } else if (queryResults.length === 0) {
+      return NextResponse.json({ items: [], cursor: null }, { status: 200 });
     }
-    return NextResponse.json(
-      { items: secondQueryResults, cursor: myCursor },
-      { status: 200 }
-    );
   } catch (error) {
     return NextResponse.json(
       { message: "Internal server error" },
