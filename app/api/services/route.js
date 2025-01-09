67c25560b6c6bed0caa437e68729a947400a68d0
AryanYadagiri <aryanyadagiri15@gmail.com>
2025-01-09 04:41:38 -0800

    improvements and changes

--- app/api/services/route.js
+++ app/api/services/route.js
@@ -7,28 +7,52 @@ export async function GET(request) {
     const category = searchParams.get("category");
     const whereClause = category === "all" ? {} : { categories: category };
     const cursor = searchParams.get("cursor");
+    const button = searchParams.get("buttom");
 
-    const queryResults = await prisma.service.findMany({
-      take: 8,
-      where: whereClause,
-      orderBy: {
-        id: "asc",
-      },
-      ...(cursor ? { cursor: { id: cursor }, skip: 1 } : {}),
-    });
-    if (queryResults.length === 8) {
-      const myCursor = queryResults[7].id;
-      return NextResponse.json(
-        { items: queryResults, cursor: myCursor },
-        { status: 200 }
-      );
-    } else if (queryResults.length < 8 && queryResults.length > 0) {
-      return NextResponse.json(
-        { items: queryResults, cursor: cursor },
-        { status: 200 }
-      );
-    } else if (queryResults.length === 0) {
-      return NextResponse.json({ items: [], cursor: null }, { status: 200 });
+    if (button === "next") {
+      const queryResults = await prisma.service.findMany({
+        take: 8,
+        where: whereClause,
+        select: { service_name: true, charges: true },
+        orderBy: {
+          id: "asc",
+        },
+        ...(cursor ? { cursor: { id: cursor }, skip: 1 } : {}),
+      });
+      if (queryResults.length === 8) {
+        const myCursor = queryResults[7].id;
+        return NextResponse.json(
+          { items: queryResults, cursor: myCursor },
+          { status: 200 }
+        );
+      } else if (queryResults.length < 8) {
+        return NextResponse.json(
+          { items: queryResults, cursor: cursor },
+          { status: 200 }
+        );
+      }
+    } else if (button === "previous") {
+      const queryResults = await prisma.service.findMany({
+        take: -8,
+        where: whereClause,
+        select: { service_name: true, charges: true },
+        orderBy: {
+          id: "asc",
+        },
+        ...(cursor ? { cursor: { id: cursor }, skip: 1 } : {}),
+      });
+      if (queryResults.length === 8) {
+        const myCursor = queryResults[0].id;
+        return NextResponse.json(
+          { items: queryResults, cursor: myCursor },
+          { status: 200 }
+        );
+      } else if (queryResults.length < 8) {
+        return NextResponse.json(
+          { items: queryResults, cursor: cursor },
+          { status: 200 }
+        );
+      }
     }
   } catch (error) {
     return NextResponse.json(
