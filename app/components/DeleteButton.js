25d705a11e59a0470455d8e8d32f8d743d4f6bac
AryanYadagiri <aryanyadagiri15@gmail.com>
2025-01-23 12:09:06 +0530

    delete button

---
+++ app/components/DeleteButton.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function DeleteButton() {
+  return (
+    <button
+    className="rounded bg-red-600 px-6 pb-2 pt-2.5 text-xs uppercase text-white hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 ...">
+    delete
+  </button>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
-import Button from "./components/Button";
+import DeleteButton from "./components/DeleteButton";
 import { Card } from "./components/Card";
 export default function Home() {
   return (
     <>
-    <div className="grid grid-cols-1 sm:grid-cols-4 gap-y-10 py-10 justify-items-center">
-
-    <Button/> 
-
-    </div>
+      <div className="grid grid-cols-1 sm:grid-cols-4 gap-y-10 py-10 justify-items-center">
+        <DeleteButton />
+      </div>
     </>
   );
 }
