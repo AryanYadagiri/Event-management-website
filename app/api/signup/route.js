eee418e6e80d48d67aa6439e36b3b7e8aa8c6d70
AryanYadagiri <aryanyadagiri15@gmail.com>
2025-01-24 18:45:10 +0530

    user registration work

--- app/api/signup/route.js
+++ app/api/signup/route.js
@@ -4,31 +4,40 @@ import bcrypt from "bcryptjs";
 
 export async function POST(request) {
   try {
-    const checker = await prisma.user.findUnique({
-      where: { email: request.body.email },
+    const req = await request.json();
+    const check1 = await prisma.user.findUnique({
+      where: { email: req.email },
     });
-    console.log(checker)
-    if (checker) {
+    const check2 = await prisma.user.findUnique({
+      where: { phone_number: req.phone_number },
+    });
+    
+    if (check1) {
       return NextResponse.json(
         { message: "Email already used." },
         { status: 409 }
       );
+    } else if (check2) {
+      return NextResponse.json(
+        { message: "Phone number already used." },
+        { status: 409 }
+      );
     }
 
-    const hashed_password = await bcrypt.hash(request.body.password);
+    const hashed_password = await bcrypt.hash(req.password, 10);
 
     await prisma.user.create({
       data: {
-        first_name: request.body.first_name,
-        last_name: request.body.last_name,
-        phone_number: request.body.phone_number,
-        email: request.body.email,
+        first_name: req.first_name,
+        last_name: req.last_name,
+        phone_number: req.phone_number,
+        email: req.email,
         hashed_password: hashed_password,
-        address: request.body.address,
-        district: request.body.district,
-        city: request.body.city,
-        state: request.body.state,
-        pincode: request.body.pincode,
+        address: req.address,
+        district: req.district,
+        city: req.city,
+        state: req.state,
+        pincode: req.pincode,
       },
     });
     return NextResponse.json(
