de0022133fbfdb54d8f76fc5df349bc2b399bb36
UzairAsi <uzairasi612@gmail.com>
2025-01-28 01:38:59 +0530

    Added Google Sign In

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import bcrypt from "bcryptjs";
 import prisma from "@/utils";
 import { NextResponse } from "next/server";
@@ -10,6 +11,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
     }),
     Credentials({
       credentials: {
