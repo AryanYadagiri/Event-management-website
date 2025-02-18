728514046cab4628fa88ba9cfa17154f5763fc9a
AryanYadagiri <aryanyadagiri15@gmail.com>
2025-02-18 10:42:54 +0530

    middleware

--- middleware.js
+++ middleware.js
@@ -1,1 +1,17 @@
-export { auth as middleware } from "@/auth"
+export { auth as middleware } from "@/auth";
+import { NextResponse } from "next/server";
+import { getSession } from "next-auth/react";
+
+export function middleware(request) {
+  const isAuthenticated = getSession();
+
+  if (isAuthenticated) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(new URL("/login", request.url));
+}
+
+export const config = {
+  matcher: "/dashboard/:path*",
+};
