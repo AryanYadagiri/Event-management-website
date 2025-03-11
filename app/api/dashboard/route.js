1fce1d8d4df6070c19ff59a1b3da9ae9d3648d6b
AryanYadagiri <aryanyadagiri15@gmail.com>
2025-03-11 18:15:26 +0530

    minor changes

--- app/api/dashboard/route.js
+++ app/api/dashboard/route.js
@@ -2,13 +2,14 @@ import prisma from "@/utils";
 import { NextResponse } from "next/server";
 import { auth } from "@/auth";
 
-export async function POST(request) {
+export  const POST = auth( async function POST(request) {
   try {
     const req = await request.json();
-    const session = await auth();
+    // const session = await auth();
+    console.log(request);
     await prisma.service.create({
       data: {
-        event_vendor_id: session.user.id,
+        event_vendor_id: request.auth.user.id,
         service_name: req.service_name,
         service_description: req.service_description,
         charges: req.charges,
@@ -20,52 +21,53 @@ export async function POST(request) {
       { status: 201 }
     );
   } catch (error) {
+    console.error("Error in creating service:", error.message || "Unknown error");
     return NextResponse.json(
       { message: "Internal server error" },
       { status: 500 }
     );
   }
-}
+})
 
-export async function PUT(request) {
-  try {
-    const req = await request.json();
-    if (req.new_service_name) {
-      await prisma.service.update({
-        where: { service_id: req.service_id },
-        data: { service_name: req.new_service_name },
-      });
-    } else if (req.new_service_description) {
-      await prisma.service.update({
-        where: { service_id: req.service_id },
-        data: { service_description: req.new_service_description },
-      });
-    } else if (req.new_charges) {
-      await prisma.service.update({
-        where: { service_id: req.service_id },
-        data: { charges: req.new_charges },
-      });
-      return NextResponse.json({ message: "Service updated" }, { status: 200 });
-    }
-  } catch (error) {
-    return NextResponse.json(
-      { message: "Internal server error" },
-      { status: 500 }
-    );
-  }
-}
+// export async function PUT(request) {
+//   try {
+//     const req = await request.json();
+//     if (req.new_service_name) {
+//       await prisma.service.update({
+//         where: { service_id: req.service_id },
+//         data: { service_name: req.new_service_name },
+//       });
+//     } else if (req.new_service_description) {
+//       await prisma.service.update({
+//         where: { service_id: req.service_id },
+//         data: { service_description: req.new_service_description },
+//       });
+//     } else if (req.new_charges) {
+//       await prisma.service.update({
+//         where: { service_id: req.service_id },
+//         data: { charges: req.new_charges },
+//       });
+//       return NextResponse.json({ message: "Service updated" }, { status: 200 });
+//     }
+//   } catch (error) {
+//     return NextResponse.json(
+//       { message: "Internal server error" },
+//       { status: 500 }
+//     );
+//   }
+// }
 
-export async function DELETE(request) {
-  try {
-    const req = await request.json();
-    await prisma.service.delete({
-      where: { service_id: req.service_id },
-    });
-    return NextResponse.json({ message: "Service deleted" }, { status: 204 });
-  } catch (error) {
-    return NextResponse.json(
-      { message: "Internal server error" },
-      { status: 500 }
-    );
-  }
-}
+// export async function DELETE(request) {
+//   try {
+//     const req = await request.json();
+//     await prisma.service.delete({
+//       where: { service_id: req.service_id },
+//     });
+//     return NextResponse.json({ message: "Service deleted" }, { status: 204 });
+//   } catch (error) {
+//     return NextResponse.json(
+//       { message: "Internal server error" },
+//       { status: 500 }
+//     );
+//   }
+// }
